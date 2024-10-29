8fe5023e457ed6d1ad2a23afdc4511ba3e10594a
đạt hoàng <153180946+datzero-9@users.noreply.github.com>
2024-10-29 16:54:26 +0700

    Update api.js

--- src/components/Helper/api.js
+++ src/components/Helper/api.js
@@ -1,5 +1,5 @@
 
 // const api = 'http://192.168.1.191:80/admin'
-const api = 'http://restfulapi-aci6.onrender.com/admin'
+const api = 'https://restfulapi-aci6.onrender.com/admin'
 export default api;
 
